54e5777257f5a89c76ddf5c11f0cf840ca624855
Dinesh nawani <nawanidinesh08@gmail.com>
2025-01-25 23:45:25 +0530

    Add files via upload

---
+++ src/component/api/API.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const PostAPI ='https://history-page-backend-x2dq.vercel.app/api/user/createImg'
+const GetAPI ='https://history-page-backend-x2dq.vercel.app/api/user/getAllUsers'
+
+export default { PostAPI, GetAPI };
